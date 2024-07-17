a0b8b873c76c3a9528e82823ddcb15bf5e7cb146
Askarbek001 <damirbekuuluaskarbek77@gmail.com>
2024-07-17 11:15:23 +0600

    Абрамян

---
+++ Задачи/script.js
@@ -0,0 +1,52 @@
+// let  a= 25
+// let z = 4
+// let p = a*z
+// let s = a*a
+// console.log(p + " периметр" +" "+s+" площад");
+
+
+// let t = 25
+// let y = 20
+// let u = 2*(t+y)
+// let i = t*y
+// console.log(u + " периметр" +" "+i+" площад");
+
+
+// let d = 50
+// let pi = 3.14
+// console.log(d*pi + " длина окружности" );
+
+
+// let reb = 15
+// let ob = reb*reb*reb
+// let pl = 6*(reb*reb)
+// console.log(ob + " объем" +" "+pl+" площад");
+
+
+// let q = 10
+// let w = 15
+// let e = 20
+// let V = q*w*e
+// let S =   2*(q*w+w*e+q*e)
+// console.log(V + " объем" +" "+S+" площад");
+
+
+// let R = 12
+// let PI = 3.14
+// let T = 2*PI*R
+// let Y = PI*(R*R)
+// console.log(T + " длина" +" "+Y+" площад");
+
+
+// let qw = 42
+// let as = 998
+// let zx = (qw+as)/2
+// console.log(zx);
+
+
+// let qwe = 20
+// let asd = 80
+// let zxc = Math.sqrt(qwe+asd)
+// console.log(zxc);
+
+
